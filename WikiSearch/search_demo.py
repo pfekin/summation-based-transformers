7240dea4a7c9a0b64caa324a4533db134c6587cf
pfekin <pfekin@gmail.com>
2025-02-26 11:54:46 -0500

    Update search_demo.py

--- WikiSearch/search_demo.py
+++ WikiSearch/search_demo.py
@@ -20,15 +20,13 @@ while True:
     for token in tokens:
         if word2index.get(token) is not None:
             query.append(word_vector[word2index[token]])
-    query_vector = np.sum(query, axis=0).reshape(1, word_vector[0].shape[0])
-    results = distance.cdist(query_vector, doc_vector, metric='cosine')[0] # cosine
-    best_results = np.argsort(results)[:20]
+    if len(query) > 0:
+        query_vector = np.sum(query, axis=0).reshape(1, word_vector[0].shape[0])
+        results = distance.cdist(query_vector, doc_vector, metric='cosine')[0] # cosine
+        best_results = np.argsort(results)[:20]
     
-    # show results
-    for i in range(len(best_results)):
-        idx = best_results[i]
-        print('[{:>5.5}]   '.format(results[idx]) + titles[idx][:-1])
+        # show results
+        for i in range(len(best_results)):
+            idx = best_results[i]
+            print('[{:>5.5}]   '.format(results[idx]) + titles[idx][:-1])
   
-
-
-    
